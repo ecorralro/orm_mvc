cbd228830417bdecef6a33f83685d2f033b3bc57
ecorralro <ecorralr689@alumnos.imf.com>
2023-12-21 18:36:55 +0100

    Create Jugadores

---
+++ modelo/jugador.py
@@ -0,0 +1,9 @@
+class Jugador():
+    def __init__(self,posx,posy,radio,direccion,entidad):
+        self.posx = posx
+        self.posy = posy
+        self.radio = radio
+        self.direccion = direccion
+        self.entidad = entidad
+
+
